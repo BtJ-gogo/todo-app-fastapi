eea55f080db4754ac8cc8daaa6de2d71bd4a1ec8
BtJ <ymawro@gmail.com>
2025-11-17 16:48:15 +0900

    feat: change column settings

--- app/models.py
+++ app/models.py
@@ -1,14 +1,14 @@
 from datetime import date
 
-from sqlalchemy import Date, func
+from sqlalchemy import Date, Integer, String, Boolean, func
 from sqlalchemy.orm import Mapped, mapped_column
 from app.database import Base
 
 class Todo(Base):
     __tablename__ = "todos"
 
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    task: Mapped[str]  = mapped_column()
-    completed: Mapped[bool] = mapped_column(default=False)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    task: Mapped[str]  = mapped_column(String(255), nullable=False)
+    completed: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
     due_date: Mapped[date] = mapped_column(Date)
     created_at: Mapped[date] = mapped_column(Date, server_default=func.date('now'))
